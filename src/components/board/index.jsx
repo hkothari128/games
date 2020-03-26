2cfaaddfd851556e5684bc044c4c4fc31f3dbb55
Huzaifa <hikothari128@gmail.com>
2020-03-26 14:49:20 +0000

    adds test to game board

--- src/components/board/index.jsx
+++ src/components/board/index.jsx
@@ -4,14 +4,19 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 const Board = ({ rows, columns }) => {
+
+  const clickHandler = (e) => {
+    console.log(JSON.parse(e.target.getAttribute('value')));
+  }
+
   return (
     <div className="board">
       {
-        Array(rows).fill(0).map(() => (
-          <div className="board__row">
+        Array(columns).fill(0).map((_, idx_row) => (
+          <div key={ idx_row } className="board__row">
             {
-              Array(columns).fill(0).map(() => (
-                <div className="board__slot"/>
+              Array(rows).fill(0).map((_, idx_slot) => (
+                <div key= { idx_slot } value={ JSON.stringify({ row: idx_row , slot: idx_slot }) } className="board__slot" onClick={clickHandler}/>
               ))
             }  
           </div>
---
+++ tests/components/board.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Board from '../../src/components/board';
+
+describe('Board', () => {
+  it('creates a Board div', () => {
+    const { container } = render(<Board />);
+  
+    expect(container.firstChild.classList.contains('board')).toBe(true);
+  });
+
+  describe('When no props given', () => {
+    it('creates a board with 8 rows and 8 slots in each row', () => {
+      const { container } = render(<Board />);
+
+      expect(container.getElementsByClassName('board__row').length).toBe(8);
+      expect(container.getElementsByClassName('board__slot').length).toBe(8*8);
+    });
+  });
+
+  it('creates a board with r rows and c slots for each row', () => {
+    const r = 10;
+    const c = 6;
+    const { container } = render(<Board rows={ r } columns={ c } />);
+
+    expect(container.getElementsByClassName('board__row').length).toBe(r);
+    expect(container.getElementsByClassName('board__slot').length).toBe(r*c);
+  });
+  
+});
