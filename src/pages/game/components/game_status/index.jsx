27f6a1f78f53381eb261e7c43198254a554fcad5
Huzaifa <hikothari128@gmail.com>
2020-05-06 11:19:50 +0100

    added player 1 and 2 names to status

--- src/pages/game/components/game_status/index.jsx
+++ src/pages/game/components/game_status/index.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 // import './styles.scss';
 
 const Status = ({ winner, playerId, compId, compName }) => {
-
-  const playerName = playerId === compId ? `Computer (${compName})` : 'Player';
+  const playerName = compId === 0 ? `Player ${playerId}`: playerId === compId ? `Computer (${compName})` : 'Player';
 
   return (
     <div className="game__status">
